aa9c20238ef701d998ef21781be43f35641a42ba
Davis Weimer <davisweimer@gmail.com>
2023-09-05 11:39:52 -0600

    feat: BCrypt engaged and password_digest -> User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :party_users
   has_many :parties, through: :party_users
 
-  validates :name, presence: true
+  validates :name, :password, presence: true
   validates :email, uniqueness: true, presence: true
+
+  has_secure_password
 end
